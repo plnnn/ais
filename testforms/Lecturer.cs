c3e452f1558d59fc672c51ac86e7cf48694d4269
Pavlo Nychyporuk <97783668+plnnn@users.noreply.github.com>
2023-12-20 15:03:50 +0200

    Update Lecturer.cs

--- testforms/Lecturer.cs
+++ testforms/Lecturer.cs
@@ -13,25 +13,10 @@ namespace bebra
         public Lecturer(string ID, string username, string password, string name, string surname)
          : base(ID, username, surname, name, surname) { }
 
-        public Lecturer(User user)
-         : base(user.getID(), user.getUsername(), user.getPassword(), user.getName(), user.getSurname()) { }
-
         public void AddGrade(Grade grade)
         {
             QueriesHandler queriesHandler = new();
             queriesHandler.AddGradeQuery(grade);
-        }
-
-        public string GetGrade(string courseID, string studentID)
-        {
-            QueriesHandler queriesHandler = new();
-            return queriesHandler.GetGradeQuery(courseID, studentID);
-        }
-
-        public void EditGrade(Grade newGrade)
-        {
-            QueriesHandler queriesHandler = new();
-            queriesHandler.EditGradeQuery(newGrade);
         }
     }
 }
